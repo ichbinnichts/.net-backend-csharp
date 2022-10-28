d30d814f5876d8fba15a5fc9786056f224c87f34
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-10-28 10:10:18 -0300

    feat: add model and repository

---
+++ Entities/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace Catalog.Entities
+{
+    //This is a model
+    public record Item
+    {
+        //Id of the model
+        public Guid Id { get; set; }
+        //Name attribute
+        public string Name { get; set; }
+        //Price attribute
+        public decimal Price { get; set; }
+        //Created date attribute
+        public DateTime CreatedDate { get; set; }   
+    }
+}
---
+++ Repository/InMemItemsRepository.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repository
+{
+    public class InMemItemsRepository
+    {
+        //Create a bunch of items
+        private readonly List<Item> items = new()
+        {
+            new Item {Id=Guid.NewGuid(), Name= "Potion", Price=9, CreatedDate = DateTime.Now},
+            new Item {Id=Guid.NewGuid(), Name= "Iron Sword", Price=20, CreatedDate = DateTime.Now},
+            new Item {Id=Guid.NewGuid(), Name= "Bronze Shield", Price=10, CreatedDate = DateTime.Now}
+        };
+        //Get all items
+        public IEnumerable<Item> GetItems()
+        {
+            return this.items;
+        }
+        //Get only one item
+        public Item GetItem(Guid id)
+        {
+            return (Item)items.Where(item => item.Id == id);
+        }
+    }
+}
