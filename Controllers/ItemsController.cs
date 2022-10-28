db8023f8669790cbff5285c66249e174b27bd125
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-10-28 10:30:31 -0300

    feat: add controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Entities;
+using Catalog.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    //Controller
+    [ApiController]
+    [Route("items")]
+    public class ItemsController: ControllerBase
+    {
+        //Getting our repository
+        private readonly InMemItemsRepository repository;
+        //Constructor
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+        //Get all items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return (IEnumerable<Item>)this.repository.GetItems();
+        }
+    }
+}
