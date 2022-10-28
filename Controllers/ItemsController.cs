8dc727e79f25dd72fbeca8cf51c4c8c789366c4b
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-10-28 10:44:39 -0300

    feat: add get item in controller

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -11,16 +11,31 @@ namespace Catalog.Controllers
     {
         //Getting our repository
         private readonly InMemItemsRepository repository;
-        //Constructor
+
+
+        // ----- Constructor -----
         public ItemsController()
         {
             repository = new InMemItemsRepository();
         }
+        // ----- Constructor END -----
+
+
         //Get all items
+
+        //GET /items
         [HttpGet]
         public IEnumerable<Item> GetItems()
         {
             return (IEnumerable<Item>)this.repository.GetItems();
+        }
+        //Get only one item
+
+        //GET /item/id
+        [HttpGet("{id}")]
+        public Item GetItem(Guid id)
+        {
+            return this.repository.GetItem(id);
         }
     }
 }
