bf0b7c4d543a8d3ccdc0daffcb6b0f3346d64044
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-10-29 13:39:36 -0300

    feat: add required notation to addItemDto

--- Dtos/AddItemDto.cs
+++ Dtos/AddItemDto.cs
@@ -1,10 +1,15 @@
-﻿namespace Catalog.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Dtos
 {
     public record AddItemDto
     {
         //Name attribute
+        [Required]
         public string Name { get; init; }
         //Price attribute
+        [Required]
+        [Range(1, 10000)]
         public decimal Price { get; init; }
         //Created date attribute
     }
