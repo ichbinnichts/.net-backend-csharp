8d41e646e1172e2e615378bb683279d7ec432e7c
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-11-03 14:09:22 -0300

    feat: add mongodbrepository class

---
+++ Repository/MongoDbItemsRepository.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repository
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        public void Add(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
